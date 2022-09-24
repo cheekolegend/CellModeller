33bb8ac3294247ae186775bedaf80c27a0a678c6
cheekolegend <aaronyip.ca@gmail.com>
2022-09-24 08:01:17 -0400

    add summary statistics calculations, change dist calc and prior definition

--- Models/biophysics_calibration/simulation_module.py
+++ Models/biophysics_calibration/simulation_module.py
@@ -7,6 +7,8 @@ import math
 
 gamma = 10
 reg_param = 0.1 
+dt = 0.02
+sim_time = 16.0
 
 def setup(sim):
     # Set biophysics module
@@ -25,32 +27,31 @@ def setup(sim):
     sim.addRenderer(therenderer)
     
     # Specify how often data is saved
-    sim.pickleSteps = 100
-    sim.dt = 0.025 #h
+    sim.pickleSteps = 10
+    sim.dt = dt #h
 
 def init(cell):
     # Specify mean and distribution of initial cell size
     cell.targetVol = 3.5 + random.uniform(0.0,0.5)
     # Specify growth rate of cells
     cell.growthRate = 1.0
-    cell.color = (0.0,1.0,0.0)
+    cell.strainRate_rolling = 0
+    cell.color = (0, 1, 0) 
 
 def update(cells):
     #Iterate through each cell and flag cells that reach target size for division
     for (id, cell) in cells.items():
-        if cell.strainRate < 0.01:
-            cell.color = (1,0,0)
-            #print(cell.cellAge)
-        else:
-            cell.color = (0,1,0)
         if cell.volume > cell.targetVol:
             cell.divideFlag = True
+        
+        if cell.cellAge != 1:    
+            cell.color = (0, cell.strainRate_rolling/dt/cell.growthRate, 0)
 
 def divide(parent, d1, d2):
     # Specify target cell size that triggers cell division
     d1.targetVol = 3.5 + random.uniform(0.0,0.5)
     d2.targetVol = 3.5 + random.uniform(0.0,0.5)
-    
+        
 def setparams(param_dict):
     global gamma, reg_param
     gamma = param_dict['gamma']
